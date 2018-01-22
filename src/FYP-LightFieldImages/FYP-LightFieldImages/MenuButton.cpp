a48bb00f62b2b08c8ff148e5a12ff077bc8d5a16
JoachimAA <joachimarthey@hotmail.co.uk>
2018-01-22 13:42:32 +0000

    added menubutton class

---
+++ src/FYP-LightFieldImages/FYP-LightFieldImages/MenuButton.cpp
@@ -0,0 +1,15 @@
+#i7nclude "MenuButton.h"
+
+
+
+MenuButton::MenuButton()
+{
++
+78
++/568*9-/581*/-
+
+
+MenuButton::~MenuButton()
+{
+}
+82
--- src/TransformsAndDatafiles/SoundManager.cpp
+++ src/TransformsAndDatafiles/SoundManager.cpp
@@ -15,6 +15,8 @@ void SoundManager::loadSound()
 
 void SoundManager::playSound()
 {
+	//sound stored in sound buffer and set to a sound 
 	m_sound.setBuffer(m_gameSound);
+	//sound used to play the buffer
 	m_sound.play();
 }
